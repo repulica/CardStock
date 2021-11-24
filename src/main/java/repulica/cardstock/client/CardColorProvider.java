18a319951c4e3b5b1b287170aaff6f4c0777268b
repulica <repulica@protonmail.com>
2021-11-24 01:49:13 -0800

    holofoil responds to more situations now

--- src/main/java/repulica/cardstock/client/CardColorProvider.java
+++ src/main/java/repulica/cardstock/client/CardColorProvider.java
@@ -2,6 +2,8 @@ package repulica.cardstock.client;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 
@@ -18,6 +20,34 @@ public class CardColorProvider implements ItemColorProvider {
 		float yaw;
 		if (holderYawValid) {
 			yaw = holderYaw;
+			if (MinecraftClient.getInstance().player != null) {
+				float playerYaw = MinecraftClient.getInstance().player.yaw;
+				if (yaw >= playerYaw) {
+					yaw -= playerYaw;
+				} else {
+					yaw = 360 - (playerYaw - yaw);
+				}
+			}
+		} else if (stack.getHolder() instanceof ItemEntity) {
+			yaw = (float) Math.toDegrees(((ItemEntity) stack.getHolder()).method_27314(MinecraftClient.getInstance().getTickDelta())) % 360F;
+			if (MinecraftClient.getInstance().player != null) {
+				float playerYaw = MinecraftClient.getInstance().player.yaw;
+				if (yaw >= playerYaw) {
+					yaw -= playerYaw;
+				} else {
+					yaw = 360 - (playerYaw - yaw);
+				}
+			}
+		} else if (stack.getHolder() != null) {
+			yaw = stack.getHolder().yaw;
+			if (MinecraftClient.getInstance().player != null) {
+				float playerYaw = MinecraftClient.getInstance().player.yaw;
+				if (yaw >= playerYaw) {
+					yaw -= playerYaw;
+				} else {
+					yaw = 360 - (playerYaw - yaw);
+				}
+			}
 		} else if (MinecraftClient.getInstance().player != null) {
 			yaw = MinecraftClient.getInstance().player.yaw;
 		} else {
